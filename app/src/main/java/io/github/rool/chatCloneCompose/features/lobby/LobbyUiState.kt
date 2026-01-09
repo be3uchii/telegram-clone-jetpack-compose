3103dfcb448c5a14f111a8b5f1ffee9219c3251f
be3uchii <standofd2axselboltnice19@gmail.com>
2026-01-09 09:19:40 +0300

    Update LobbyUiState.kt

--- app/src/main/java/io/github/rool/chatCloneCompose/features/lobby/LobbyUiState.kt
+++ app/src/main/java/io/github/rool/chatCloneCompose/features/lobby/LobbyUiState.kt
@@ -1,14 +1,8 @@
 package io.github.rool.chatCloneCompose.features.lobby
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Group
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Man
-import androidx.compose.material.icons.filled.NearMe
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.filled.VerifiedUser
 import androidx.compose.ui.graphics.vector.ImageVector
 import io.github.rool.chatCloneCompose.core.models.Chat
 
@@ -20,14 +14,8 @@ data class LobbyUiState(
 ) {
     object MockedState {
         private fun mockedDrawerItem(): List<DrawerItem> = listOf(
-            DrawerItem(Icons.Filled.Group, "New group"),
-            DrawerItem(Icons.Filled.Person, "Contacts"),
-            DrawerItem(Icons.Filled.Call, "Calls"),
-            DrawerItem(Icons.Filled.NearMe, "Near me"),
             DrawerItem(Icons.Filled.Man, "Saved messages"),
-            DrawerItem(Icons.Filled.Settings, "Settings"),
-            DrawerItem(Icons.Filled.VerifiedUser, "Invite friends"),
-            DrawerItem(Icons.Filled.Info, "About me")
+            DrawerItem(Icons.Filled.Settings, "Settings")
         )
 
         fun lobbyUiMockedState(): LobbyUiState =
